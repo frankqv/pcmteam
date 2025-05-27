4412724fda2c5d04d373efbe6f620cb195917fa4
frankqv <mr.qnns008@gmail.com>
2025-05-27 15:10:23 -0500

    Aprovecha cada día como una oportunidad para crecer y aprender algo nuevo.

--- frontend/login.php
+++ frontend/login.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GET THE BEST</title>
+    <title>PCMARKETTEAM</title>
     <link rel="stylesheet" href="../backend/css/style.css">
     <link rel="icon" type="image/png" href="../backend/img/favicon.png"/>
 </head>
