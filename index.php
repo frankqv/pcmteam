88ebaacceb9c3a765a5ef0dbb322a83ac2e47547
frankqv <mr.qnns008@gmail.com>
2025-09-10 17:43:58 -0500

    La experiencia no se equivoca; sólo tus juicios se equivocan al esperar de ella lo que no está en su poder.
    Leonardo Da Vinci
    
    Deberíamos crear una cátedra para enseñar a leer entre líneas.
    
    León Bloy

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Redirigir al login
+header('Location: public_html/home.php');
+exit;
+?>
--- public_html/layouts/menu_data.php
+++ public_html/layouts/menu_data.php
@@ -126,6 +126,13 @@ if (in_array($rol, [1, 3, 4, 5, 6, 7])) {
 }
 if (in_array($rol, [1, 3, 4, 5, 6, 7])) {
     $ventaItems[] = [
+        'label' => 'Clientes',
+        'icon' => 'groups',
+        'url' => '../venta/mostrar2.php'
+    ];
+}
+if (in_array($rol, [1, 3, 4, 5, 6, 7])) {
+    $ventaItems[] = [
         'label' => 'VENDER',
         'icon' => 'request_page',
         'url' => '../venta/mostrar2.php'
